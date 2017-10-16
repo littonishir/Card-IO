522f46e9183c94f5f60860844a0a03b5edc3ff3d
littonishir <littonishir@gmail.com>
2017-10-16 19:28:00 +0800

    Update MainActivity.java

--- src/main/java/com/example/cardiodemo/MainActivity.java
+++ src/main/java/com/example/cardiodemo/MainActivity.java
@@ -1,15 +1,17 @@
 package com.example.cardiodemo;
 
+import android.Manifest;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import io.card.payment.CardIOActivity;
-import io.card.payment.CardType;
 import io.card.payment.CreditCard;
 
 public class MainActivity extends AppCompatActivity {
